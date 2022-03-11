5b760e775fc113a3a4b05f757657b0f9f9d264e0
Kenny Deng <kennydeng@cmail.carleton.ca>
2022-03-11 06:00:56 -0500

    ADD: Working audio recording and sending prototype

--- record_audio/record_audio.py
+++ record_audio/record_audio.py
@@ -1,4 +1,4 @@
-#mport pyaudio
+#import pyaudio
 #p = pyaudio.PyAudio()
 #for ii in range(p.get_device_count()):
 #    print(p.get_device_info_by_index(ii).get('name'))
@@ -12,7 +12,7 @@ samp_rate = 44100 # 44.1kHz sampling rate
 chunk = 4096 # 2^12 samples for buffer
 record_secs = 3 # seconds to record
 dev_index = 1 # device index found by p.get_device_info_by_index(ii)
-wav_output_filename = 'test2.wav' # name of .wav file
+wav_output_filename = 'test0.wav' # name of .wav file
 
 audio = pyaudio.PyAudio() # create pyaudio instantiation
 
@@ -20,7 +20,7 @@ audio = pyaudio.PyAudio() # create pyaudio instantiation
 stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
                     input_device_index = dev_index,input = True, \
                     frames_per_buffer=chunk)
-print("recording")
+print("started recording")
 frames = []
 
 # loop through stream and append audio chunks to frame array
