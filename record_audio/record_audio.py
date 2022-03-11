f4360f48e658441bd51422e385a4ff7952826a05
Kenny Deng <kennydeng@cmail.carleton.ca>
2022-03-11 05:04:33 -0500

    Test pyaudio record output again

--- record_audio/record_audio.py
+++ record_audio/record_audio.py
@@ -2,37 +2,28 @@ import pyaudio
 import wave
  
 FORMAT = pyaudio.paInt16
-CHANNELS = 1
+CHANNELS = 2
 RATE = 44100
-CHUNK = 512
+CHUNK = 1024
 RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "recordedFile.wav"
-device_index = 2
+WAVE_OUTPUT_FILENAME = "file.wav"
+ 
 audio = pyaudio.PyAudio()
-
-print("----------------------record device list---------------------")
-info = audio.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
-for i in range(0, numdevices):
-        if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
-
-print("-------------------------------------------------------------")
-
-index = int(input())
-print("recording via index "+str(index))
-
+ 
+# start Recording
 stream = audio.open(format=FORMAT, channels=CHANNELS,
-                rate=RATE, input=True,input_device_index = index,
+                rate=RATE, input=True,
                 frames_per_buffer=CHUNK)
-print ("recording started")
-Recordframes = []
+print("recording...")
+frames = []
  
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
-    Recordframes.append(data)
-print ("recording stopped")
+    frames.append(data)
+print("finished recording")
  
+ 
+# stop Recording
 stream.stop_stream()
 stream.close()
 audio.terminate()
@@ -41,5 +32,5 @@ waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 waveFile.setnchannels(CHANNELS)
 waveFile.setsampwidth(audio.get_sample_size(FORMAT))
 waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(Recordframes))
+waveFile.writeframes(b''.join(frames))
 waveFile.close()
