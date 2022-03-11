5b8f64974cfcd1b375f249a7b210c65f5b1ef3f8
Cristian Figueroa <cristianfigueroa@cmail.carleton.ca>
2022-03-11 12:28:54 -0500

    'Set threshold' button fully functional, 'Get_Data' button can query data but only prints it out in python

--- flask_app/myapp.py
+++ flask_app/myapp.py
@@ -3,20 +3,48 @@ Flask Server for baby monitor front end GUI
 """
 import os
 from flask import Flask, render_template, request, make_response
+from firebase import *
 
 app = Flask(__name__)
+
+# Init Variables
+db = Firebase().db
 
 @app.route('/')
 def default():
     return render_template('index.html')
 
-@app.route('/getdata')
+@app.route('/getdata', methods=['POST', 'GET'])
 def get_data():
-    return "Hello World"
+    # If user selects get data, present form
+    if request.method == 'GET':
+        return render_template('data_form.html')
+    # If user has entered sensorid, display data for that sensor
+    elif request.method == 'POST':
+        sensor_id = request.form['sensorID']
+        entries = db.child("sensors").child(sensor_id).child("entries").get().each()
+        print("Printing data for sensor #{}".format(sensor_id))
+        for entry in entries:
+            value = entry.val()["value"]
+            date = entry.val()["date"]
+            time = entry.val()["time"]
+            print("Value: {}, Date: {}, Time: {}".format(value, date, time))
+        return render_template('data_form.html')
 
-@app.route('/setthreshold')
+@app.route('/setthreshold', methods=['POST', 'GET'])
 def set_threshold():
-    return "Hello World"
+    # If user selects get data, present form
+    if request.method == 'GET':
+        return render_template('threshold_form.html')
+    # If user has entered sensorid, display data for that sensor
+    elif request.method == 'POST':
+        min_temp = request.form['min_temp']
+        max_temp = request.form['max_temp']
+        # Package data
+        data = {"min_temperature": min_temp, "max_temperature": max_temp}
+        # Update data in DB
+        db.child("sensors").child(123).update(data)
+        return render_template('threshold_form.html')
 
 @app.route('/livestream')
 def livestream():
