bf64f41676b2689ea5fec1257fd01eb15f00f594
Kenny Deng <kennydeng@cmail.carleton.ca>
2022-03-11 05:24:52 -0500

    Test pyaudio record output again againagain

--- record_audio/record_audio.py
+++ record_audio/record_audio.py
@@ -1,24 +1,44 @@
 import pyaudio
 import wave
  
-audio = pyaudio.PyAudio()
-stream = audio.open(format = pyaudio.paInt16, channels = 1, rate = 44100, input = True, frames_per_buffer = 1024)
-
-frames = []
-
-try:
-    while True:
-        data = stream.read(1024)
-        frames.append(data)
-except KeyboardInterrupt:
-    pass
-
+chunk = 1024      # Each chunk will consist of 1024 samples
+sample_format = pyaudio.paInt16      # 16 bits per sample
+channels = 2      # Number of audio channels
+fs = 44100        # Record at 44100 samples per second
+time_in_seconds = 3
+filename = "soundsample.wav"
+ 
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
+ 
+print('-----Now Recording-----')
+ 
+#Open a Stream with the values we just defined
+stream = p.open(format=sample_format,
+                channels = channels,
+                rate = fs,
+                frames_per_buffer = chunk,
+                input = True)
+ 
+frames = []  # Initialize array to store frames
+ 
+# Store data in chunks for 3 seconds
+for i in range(0, int(fs / chunk * time_in_seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+ 
+# Stop and close the Stream and PyAudio
 stream.stop_stream()
 stream.close()
-audio.terminate()
-
-sound_file = wave.open("myrecording.wav", "wb")
-sound_file.setnchannels(1)
-sound_file.setwampwidth(audio.getsample_size(pyaudio.paInt16))
-sound_file.setframerate(44100)
-sound_file.writeframes(b''.join(frames))
+p.terminate()
+ 
+print('-----Finished Recording-----')
+ 
+# Open and Set the data of the WAV file
+file = wave.open(filename, 'wb')
+file.setnchannels(channels)
+file.setsampwidth(p.get_sample_size(sample_format))
+file.setframerate(fs)
+ 
+#Write and Close the File
+file.writeframes(b''.join(frames))
+file.close()
