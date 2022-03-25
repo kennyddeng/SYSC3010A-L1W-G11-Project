8821021615aa75dbfeb5c3b332177ad9f6437e34
Kenny Deng <kennydeng@cmail.carleton.ca>
2022-03-24 20:53:06 -0400

    docs: code comment and documentation

--- detect_sound/sound_sensor.py
+++ detect_sound/sound_sensor.py
@@ -20,8 +20,8 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 # Sound Sensor Setup
-channel = 2
-GPIO.setmode(GPIO.BCM)
+channel = 2 # GPIO Number Sound Sensor is connected to
+GPIO.setmode(GPIO.BCM) # GPIO Numbering Mode (BCM)
 GPIO.setup(channel, GPIO.IN)
 
 key = 0
@@ -33,18 +33,23 @@ def writeData():
     #db.child('sensors').child('sound_sensor').child(key).set(data)
     db.child("sensors").child("sound_sensor").child("entries").child(time.strftime('D%Y-%m-%dT%H:%M:%S')).set(data)
     key += 1
-    
+
+# output to Console when high sound level detected by Sound Sensor
+# timestamp in iso8601 date-time format
 def outputEventToConsole():
     print("Sound Detected", time.strftime('D%Y-%m-%dT%H:%M:%S'))
 
+# process event and output to user/console
 def callback(channel):
     if GPIO.input(channel):
         outputEventToConsole()
         writeData()
         #time.sleep(10)
-    
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime = 300) # bouncetime is # ms between samples
-GPIO.add_event_callback(channel, callback)
+
+# When high sound level detected by Sound Sensor, Sound Sensor sends a digital signal to Raspberry Pi
+# event detects when the digital signal is sent
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime = 300) # bouncetime is number of ms between samples
+                                                            # 1000 = 1000 ms = 1 sGPIO.add_event_callback(channel, callback) # process event
 
 def loop():
     while True:
